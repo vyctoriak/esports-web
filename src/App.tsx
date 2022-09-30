6d23b064e0aa1f87d584909c81458975e4e475cc
vyctoria karina <vyctoriak@gmail.com>
2022-09-30 16:35:35 +0200

    interface content game banner calling api and iterating, stating modal code for pusblish ad"

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,29 @@ import './styles/main.css';
 import logoImg from './assets/Logo.svg';
 import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
+import { useEffect, useState } from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  };
+}
 
 function App() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then((response) => response.json())
+      .then((data) => {
+        setGames(data);
+      });
+  }, []);
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="logo" />
@@ -18,16 +39,33 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner
-          bannerUrl="/game-1.png"
-          title="League of Legends"
-          adsCount={2}
-        />
-        <GameBanner bannerUrl="/game-2.png" title="Dota 2" adsCount={1} />
-        <GameBanner bannerUrl="/game-3.png" title="CS Go" adsCount={0} />
+        {games.map((game) => {
+          return (
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          );
+        })}
       </div>
 
-      <CreateAdBanner />
+      <Dialog.Root>
+        <CreateAdBanner />
+
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
+
+          <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Dialog.Title>Publish ad</Dialog.Title>
+
+            <Dialog.Content>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </Dialog.Content>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   );
 }
